04d8b7405fa781635b54635a916c8c7cdcc72c84
rgaiser <rgaiser@9b99efca-2682-11de-ba08-0b5a79c0c79e>
2006-05-04 20:30:06 +0000

    inttypes.h added
    git-svn-id: http://bricks-os.googlecode.com/svn/trunk@5 9b99efca-2682-11de-ba08-0b5a79c0c79e

---
+++ include/posix/inttypes.h
@@ -0,0 +1,25 @@
+#ifndef INTTYPES_H
+#define INTTYPES_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+typedef char               int8_t;
+typedef short              int16_t;
+typedef long               int32_t;
+typedef long long          int64_t;
+typedef unsigned char      uint8_t;
+typedef unsigned short     uint16_t;
+typedef unsigned long      uint32_t;
+typedef unsigned long long uint64_t;
+typedef int                intptr_t
+typedef unsigned int       uintptr_t
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
