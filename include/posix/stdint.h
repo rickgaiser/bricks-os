77bb37063f0bd14b0d1254e2500e8703c08d5f77
rgaiser <rgaiser@9b99efca-2682-11de-ba08-0b5a79c0c79e>
2007-10-01 21:09:25 +0000

    Quick fix for ps2, file needs to be arch dependent though.
    git-svn-id: http://bricks-os.googlecode.com/svn/trunk@164 9b99efca-2682-11de-ba08-0b5a79c0c79e

--- include/posix/stdint.h
+++ include/posix/stdint.h
@@ -8,11 +8,11 @@ extern "C" {
 
 typedef char                        int8_t;
 typedef short                       int16_t;
-typedef long                        int32_t;
+typedef int                         int32_t;
 typedef long long                   int64_t;
 typedef unsigned char               uint8_t;
 typedef unsigned short              uint16_t;
-typedef unsigned long               uint32_t;
+typedef unsigned int                uint32_t;
 typedef unsigned long long          uint64_t;
 
 typedef int8_t                      __s8;
@@ -24,14 +24,14 @@ typedef uint16_t                    __u16;
 typedef uint32_t                    __u32;
 typedef uint64_t                    __u64;
 
-typedef volatile char               vint8_t;
-typedef volatile short              vint16_t;
-typedef volatile long               vint32_t;
-typedef volatile long long          vint64_t;
-typedef volatile unsigned char      vuint8_t;
-typedef volatile unsigned short     vuint16_t;
-typedef volatile unsigned long      vuint32_t;
-typedef volatile unsigned long long vuint64_t;
+typedef volatile int8_t             vint8_t;
+typedef volatile int16_t            vint16_t;
+typedef volatile int32_t            vint32_t;
+typedef volatile int64_t            vint64_t;
+typedef volatile uint8_t            vuint8_t;
+typedef volatile uint16_t           vuint16_t;
+typedef volatile uint32_t           vuint32_t;
+typedef volatile uint64_t           vuint64_t;
 
 typedef int                         intptr_t;
 typedef unsigned int                uintptr_t;
