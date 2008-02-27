71fa0ec18c8dfb84194bd5ba50cc9f5111d4c056
rgaiser <rgaiser@9b99efca-2682-11de-ba08-0b5a79c0c79e>
2008-02-27 20:01:10 +0000

    Add default application that does nothing
    git-svn-id: http://bricks-os.googlecode.com/svn/trunk@315 9b99efca-2682-11de-ba08-0b5a79c0c79e

---
+++ apps/halt/halt.cpp
@@ -0,0 +1,11 @@
+//#include "kernel/debug.h"
+
+
+//---------------------------------------------------------------------------
+extern "C" int
+appMain(int argc, char * argv[])
+{
+  while(1);
+
+  return 0;
+}
