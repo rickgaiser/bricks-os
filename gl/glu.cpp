718274b0637032f86781f6cec24de027f2fb6d3b
rgaiser <rgaiser@9b99efca-2682-11de-ba08-0b5a79c0c79e>
2008-05-20 08:47:17 +0000

    Add empty functions for glu mipmaps and image scaling.
    git-svn-id: http://bricks-os.googlecode.com/svn/trunk@438 9b99efca-2682-11de-ba08-0b5a79c0c79e

--- gl/glu.cpp
+++ gl/glu.cpp
@@ -5,6 +5,13 @@ typedef unsigned int wint_t;
 
 
 //-----------------------------------------------------------------------------
+int
+gluBuild2DMipmaps(GLenum target, GLint components, GLint width, GLint height, GLenum format, GLenum type, const void * data)
+{
+  return -1;
+}
+
+//-----------------------------------------------------------------------------
 void
 gluPerspective(GLdouble fovy, GLdouble aspect, GLdouble zNear, GLdouble zFar)
 {
@@ -15,6 +22,12 @@ gluPerspective(GLdouble fovy, GLdouble aspect, GLdouble zNear, GLdouble zFar)
   xmin = ymin * aspect;
   xmax = ymax * aspect;
 
-
   glFrustumf(xmin, xmax, ymin, ymax, zNear, zFar);
+}
+
+//-----------------------------------------------------------------------------
+int
+gluScaleImage(GLenum format, GLint widthin, GLint heightin, GLenum typein, const void * datain, GLint widthout, GLint heightout, GLenum typeout, void * dataout)
+{
+  return -1;
 }
--- include/GL/GL/glu.h
+++ include/GL/GL/glu.h
@@ -10,7 +10,9 @@ extern "C" {
 #endif
 
 
+int  gluBuild2DMipmaps(GLenum target, GLint components, GLint width, GLint height, GLenum format, GLenum type, const void * data);
 void gluPerspective(GLdouble fovy, GLdouble aspect, GLdouble zNear, GLdouble zFar);
+int  gluScaleImage(GLenum format, GLint widthin, GLint heightin, GLenum typein, const void * datain, GLint widthout, GLint heightout, GLenum typeout, void * dataout);
 
 
 #ifdef __cplusplus
