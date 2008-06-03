3101fd05073d27217f6a83a389cbd73db230dbae
rgaiser <rgaiser@9b99efca-2682-11de-ba08-0b5a79c0c79e>
2008-06-03 10:28:52 +0000

    Fix compilation errors with dummy functions.
    git-svn-id: http://bricks-os.googlecode.com/svn/trunk@468 9b99efca-2682-11de-ba08-0b5a79c0c79e

--- include/posix/math.h
+++ include/posix/math.h
@@ -5,4 +5,10 @@
 #include "_math.h"
 
 
+inline float sin(float angle){return 1.0f;}
+inline float cos(float angle){return 1.0f;}
+inline float tan(float angle){return 1.0f;}
+inline float sqrt(float value){return 1.0f;}
+
+
 #endif
