067bc36e6ee0690e8fce8fbe86151a89397a4640
rgaiser <rgaiser@9b99efca-2682-11de-ba08-0b5a79c0c79e>
2008-10-29 12:36:18 +0000

    Change debugging to use the new default video initialization (both ngc & wii).
    git-svn-id: http://bricks-os.googlecode.com/svn/trunk@649 9b99efca-2682-11de-ba08-0b5a79c0c79e

--- kernel/arch/powerpc/ngcwii/debugScreen.cpp
+++ kernel/arch/powerpc/ngcwii/debugScreen.cpp
@@ -25,23 +25,10 @@ CNGCDebugScreen::init()
 {
   VIDEO_Init();
 
-  switch(VIDEO_GetCurrentTvMode())
-  {
-      case VI_NTSC:
-          rmode = &TVNtsc480IntDf;
-          break;
-      case VI_PAL:
-          rmode = &TVPal528IntDf;
-          break;
-      case VI_MPAL:
-          rmode = &TVMpal480IntDf;
-          break;
-      default:
-          rmode = &TVNtsc480IntDf;
-          break;
-  }
+  rmode = VIDEO_GetPreferredMode(NULL);
 
   xfb = MEM_K0_TO_K1(SYS_AllocateFramebuffer(rmode));
+  console_init(xfb,20,20,rmode->fbWidth,rmode->xfbHeight,rmode->fbWidth*VI_DISPLAY_PIX_SZ);
 
   VIDEO_Configure(rmode);
   VIDEO_SetNextFramebuffer(xfb);
@@ -50,8 +37,6 @@ CNGCDebugScreen::init()
   VIDEO_WaitVSync();
   if(rmode->viTVMode&VI_NON_INTERLACE)
     VIDEO_WaitVSync();
-
-  console_init(xfb,20,64,rmode->fbWidth,rmode->xfbHeight,rmode->fbWidth*2);
 
   return 0;
 }
