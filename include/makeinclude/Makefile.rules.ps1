4d4d1d2cce53e658e5340c47fb176ac2724d2458
rgaiser <rgaiser@9b99efca-2682-11de-ba08-0b5a79c0c79e>
2008-10-30 08:52:03 +0000

    Add arch definitions and linker map output.
    git-svn-id: http://bricks-os.googlecode.com/svn/trunk@650 9b99efca-2682-11de-ba08-0b5a79c0c79e

--- include/makeinclude/Makefile.rules.ps1
+++ include/makeinclude/Makefile.rules.ps1
@@ -9,12 +9,27 @@ MY_LIBS_PATH    +=\
 
 #----------------------------------------------------------------------------
 CROSS_COMPILE   =mips
-CFLAGS          +=-Xlinker -mpsx
-CXXFLAGS        +=-Xlinker -mpsx
-LINK_FLAGS      +=-Xlinker -mpsx $(MY_LIBS_PATH) -lps /D/dev/env/devkitPSX/lib/htsprint.o
+CFLAGS          +=\
+                -Xlinker \
+                -mpsx \
+                -DPS1 \
+
+CXXFLAGS        +=\
+                -Xlinker \
+                -mpsx \
+                -DPS1 \
+
+LINK_FLAGS      +=\
+                -Xlinker \
+                -mpsx \
+                -Wl,-Map,$@.map \
+
+MY_LIBS         +=\
+                -lps \
+                /D/dev/env/devkitPSX/lib/htsprint.o
 
 #----------------------------------------------------------------------------
 # Create .elf file from MY_OBJS + MY_LIBS
-$(MY_ELF): $(MY_OBJS)
+$(MY_ELF): $(MY_OBJS) $(MY_LIBS_DEP)
 	@echo [CC ] $@
-	$(CXX) $(LINK_FLAGS) $^ $(MY_LIBS) -o $@
+	$(CXX) $(LINK_FLAGS) $(MY_OBJS) $(MY_LIBS_PATH) $(MY_LIBS) -o $@
